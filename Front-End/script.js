bf73724719038a806543111fa66b699fce783c61
Filipy <89490624+FilipyStudy@users.noreply.github.com>
2024-03-05 12:01:24 -0300

    Added some DOM play.
    I added a kind of "easteregg" to prove a concept about DOM with javascript. Another images and a favicon to the pages. Everything from open sources.

--- Front-End/script.js
+++ Front-End/script.js
@@ -1,3 +1,9 @@
-function onClickButton(){
-    alert("Relax, i am working on it!")
+const bodyElement = document.getElementById("bori");
+
+function onClickButton() {
+    alert("Relax, i am working on it! 😎");
+}
+
+function changeBackground(){
+    document.body.style.backgroundImage = "url('imgs/easteregg_bg.jpeg')";
 }
